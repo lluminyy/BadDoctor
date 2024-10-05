300aada54c0cc76564960ed9addfb8e76a2bace5
Barney Moss <barneym@meta.com>
2024-10-05 10:27:21 +0100

    Initial Commit

---
+++ laurie epq/laurie_epqApp.swift
@@ -0,0 +1,17 @@
+//
+//  laurie_epqApp.swift
+//  laurie epq
+//
+//  Created by Barney Moss on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct laurie_epqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ laurie epqTests/laurie_epqTests.swift
@@ -0,0 +1,17 @@
+//
+//  laurie_epqTests.swift
+//  laurie epqTests
+//
+//  Created by Barney Moss on 05/10/2024.
+//
+
+import Testing
+@testable import laurie_epq
+
+struct laurie_epqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
